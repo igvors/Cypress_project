3208fd9abd608ba87811e0d56525e588f632217a
igvors <varo6ka@gmail.com>
2021-04-07 22:02:45 +0300

    add 2 lecture code

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,41 +1,40 @@
 /// <reference types="Cypress" />
 
-// it('By ID', () => {
-//     cy.visit('https://facebook.com')
-//     cy.get('#email')
-// });
+it('By ID', () => {
+    cy.visit('https://facebook.com')
+    cy.get('#email')
+});
 
-// it('By Class', () => {
-//     cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax')
-//     cy.get('.ds-input')
-// });
+it('By Class', () => {
+    cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax')
+    cy.get('.ds-input')
+});
 
-// it('By Tag', () => {
-//     cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax')
-//     cy.get('nav')
-// });
+it('By Tag', () => {
+    cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax')
+    cy.get('nav')
+});
 
-// it('By Tag Value', () => {
-//     cy.visit('https://facebook.com')
-//     cy.get('[name="pass"]')
-// });
+it('By Tag Value', () => {
+    cy.visit('https://facebook.com')
+    cy.get('[name="pass"]')
+});
 
-// it('By Different Tag', () => {
-//     cy.visit('https://facebook.com')
-//     cy.get('[data-testid="open-registration-form-button"][role="button"]')
-// });
+it('By Different Tag', () => {
+    cy.visit('https://facebook.com')
+    cy.get('[data-testid="open-registration-form-button"][role="button"]')
+});
 
-// it('By Different Types', () => {
-//     cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax')
-//     cy.get('button[type="button"][title="close banner"]')
-// });
+it('By Different Types', () => {
+    cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax')
+    cy.get('button[type="button"][title="close banner"]')
+});
 
-// it('By Contains name', () => {
-//     cy.visit('https://next.privat24.ua/')
-//     cy.get('*[class^="card"]')
-// });
+it('By Contains name', () => {
+    cy.visit('https://next.privat24.ua/')
+    cy.get('*[class^="card"]')
+});
 
-//Comment
 it('Using Get with Find and Eq', () => {
     cy.visit('https://next.privat24.ua/deposit')
     cy.get('tbody').find('td').find('div').find('button').eq('0')
